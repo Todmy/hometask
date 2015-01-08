f53feda6dc029c6bebfd4b16ef619c56e17ccdb4
Todmy <tolok.d@gmail.com>
2015-01-08 17:33:35 +0200

    deleted false links

--- JS & jQuery v0.1/js/json-format.js
+++
@@ -1,4 +0,0 @@
-function jsonFormat() {
-    console.log(JSON.stringify(arrElems.getElement()));
-    $('[jq-app]').find('[jq-show]').html(JSON.stringify(arrElems.getElement())).toggleClass('invisible');
-};
