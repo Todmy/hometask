2698e40e9ce3a09d4ac8d62558365b9031cce0ec
Todmy <tolok.d@gmail.com>
2015-01-14 15:31:41 +0200

    fixed create and update item actions

--- JS & jQuery v0.1/js/app.js
+++ JS & jQuery v0.1/js/app.js
@@ -20,7 +20,7 @@
                 'click .modal-overlay': 'closeForm'
             });
 
-            this._appendFormItem();
+            this._appendItemForm();
             this._appendJsonPlace();
 
             this.validates('[name=item] input[name=title]', 'required')
@@ -29,36 +29,39 @@
 
         },
 
-        _appendFormItem: function () {
+        _appendItemForm: function () {
             var itemFormTemplate = $('#' + this.options.rowTemplate).text();
+
             this.element.append(itemFormTemplate);
-            this.itemForm = this.element.find('form[name=item]').get(0);
+            this.itemForm = this.element.find('form[name=item]');
         },
 
         _appendJsonPlace: function () {
-            this.jsonPlace = $('<pre class="invisible"></pre>');
-            this.element.append(this.jsonPlace);
+            this.jsonOutput = $('<pre class="invisible"></pre>');
+            this.element.append(this.jsonOutput);
         },
 
         _init: function () { //implemented separately in order to empower. If we will have only one table we can to implement the initialization in _create method
-            this.options.template = $(this.options.body).html();
+            this.options.template = this.options.body.html();
             this._createTable();
         },
 
         _createTable: function () {
             var tableRows = this.jqRepeat(this.options.list.get(), this.options.template);
-            this.options.body.text('').append(tableRows);
+
+            this.options.body.empty().append(tableRows);
         },
 
         showJson: function () {
             var dump = JSON.stringify(this.options.list.get(), null, 2);
-            this.jsonPlace.text(dump).toggleClass('invisible');
+
+            this.jsonOutput.text(dump).toggleClass('invisible');
         },
 
         deleteElement: function (event) {
             var index = $(event.target).closest('tr').index();
-            var message = 'Do you want to delete ' + this.options.list.get()[index].title +
-                '(' + this.options.list.get()[index].sku + ') element?';
+            var message = 'Do you want to delete ' + this.options.list.get(index).title +
+                '(' + this.options.list.get(index).sku + ') element?';
 
             if (confirm(message)) {
                 this._deleteRowAt(index);
@@ -66,7 +69,7 @@
         },
 
         _deleteRowAt: function (index) {
-            this.element.find('[jq-repeat] tr').eq(index).remove();
+            this.options.body.find('tr').eq(index).remove();
             this.options.list.del(index);
         },
 
@@ -77,15 +80,15 @@
 
         editElement: function (event) {
             var row = $(event.target).closest('tr');
-            this.index = row.index();
-            this.editableElementLink = this.options.list.get(this.index); //got this link only for purpose to correctly validate uniqueness sku
-            var editableElement = arrElems.get(this.index);
-            this._fillProductForm(editableElement);
+
+            this.editableElement = this.options.list.get(row.index()); //got this link only for purpose to correctly validate uniqueness sku
+            this._fillProductForm(this.editableElement);
             this._showProductForm();
         },
 
         _fillProductForm: function (currentItem) {
-            var form = this.itemForm;
+            var form = this.itemForm.get(0);
+
             currentItem = currentItem || {};
 
             $.each('title sku price'.split(' '), function (i, fieldName) {
@@ -94,8 +97,8 @@
         },
 
         _showProductForm: function () {
-            $(this.itemForm).removeClass('invisible')
-                .find('input[type=submit]').attr('value', event.target.innerHTML);
+            this.itemForm.removeClass('invisible')
+                .find('input[type=submit]').attr('value', $(event.target).attr('name'));
             this.element.find('.modal-overlay').removeClass('invisible');
         },
 
@@ -111,94 +114,115 @@
 
         _getValidators: function (validators) {
             var self = this;
+
             return function (event) {
-                validators.split(' ').forEach(function (element) {
-                    var errorObject = self.validFunctionsHolder(element, event);
-                    $(event.target).removeClass('invalid');
-                    if(!errorObject.isValid && !$(event.target).hasClass('invalid')){
-                        $(event.target).addClass('invalid');
-                        console.log(errorObject.errorMessage);
-                    } else if (errorObject.isValid && $(event.target).hasClass('invalid')) {
-                        $(event.target).removeClass('invalid');
+                validators.split(' ').forEach(function (validAttrs) {
+                    var field = $(event.target);
+                    var errorObject = self._validationRules[validAttrs].call(self, field.get(0));
+
+                    field.removeClass('invalid');
+                    if(!errorObject.exists){
+                        field.addClass('invalid');
+                        throw new Error(errorObject.errorMessage);
                     }
                 })
             }
         },
 
-        validFunctionsHolder: function (key, event) {
-            switch (key) {
-                case 'required':
-                    return {
-                        isValid: !!event.target.value,
-                        errorMessage: 'Not filled all fields'
-                    };
-                case 'unique':
-                    return {
-                        isValid: this.isUnique(event),
-                        errorMessage: 'Not unique SKU'
-                    };
-                case 'number':
-                    return {
-                        isValid: typeof (+event.target.value) === 'number',
-                        errorMessage: 'Price must be a number'
-                    };
-                default :
-                    throw new Error('An unknown validation query');
-                    /*can be added other validation queries*/
-                    break;
-            }
-        },
+        _validationRules: { /*can be added other validation queries*/
+            required: function (field) {
+                return {
+                    exists: !!field.value,
+                    errorMessage: 'Not filled all fields'
+                }
+            },
 
-        isUnique: function (event) {
-            var self = this;
-            return this.options.list.get().every(function(elem){
-                return event.target.value !== elem.sku || elem === self.editableElementLink;
-            });
+            unique: function (field) {
+                var isUnique = this.options.list.get().every(function(elem){
+                        return field.value !== elem.sku || elem === this.editableElement;
+                    }, this);
+
+                return {
+                    exists: isUnique,
+                    errorMessage: 'Not unique SKU'
+                }
+            },
+
+            number: function (field) {
+                return {
+                    exists: typeof (+field.value) === 'number',
+                    errorMessage: 'Price must be a number'
+                }
+            }
         },
 
         _submitProductForm: function (event) {
             var item = this._getNewItem();
+            var isValid = this._isValidForm();
+            var createOrUpdate = '_' + this.itemForm.find('input[type=submit]').attr('value') + 'Item';
 
             event.preventDefault();
 
-            var isValid = this._isValidForm();
-
-            if (isValid && $(this.itemForm).find('input[type=submit]').attr('value') === 'add') {
-                this._addNewItem(item);
-                this._hideForm();
-            } else if (isValid && $(this.itemForm).find('input[type=submit]').attr('value') === 'edit') {
-                this._editCurrentItem(item);
-                this._hideForm();
-                this.editableElementLink = {};
+            if(isValid){
+                this[createOrUpdate](item);
             }
         },
 
         _getNewItem: function () {
+            var item = this.itemForm.get(0);
+
             return {
-                title: this.itemForm.title.value,
-                sku: this.itemForm.sku.value,
-                price: this.itemForm.price.value
+                title: item.title.value,
+                sku: item.sku.value,
+                price: item.price.value
             };
         },
 
         _isValidForm: function () {
-            return $.makeArray($(this.itemForm).find('input')).every(function (element) {
+            return this.itemForm.find('input').toArray().every(function (element) {
                 return !$(element).hasClass('invalid');
             })
         },
 
+        _createItem: function (item) {
+            this._addNewItem(item);
+            this._hideForm();
+        },
+
+        _updateItem: function (item) {
+            this._editCurrentItem(item);
+            this._hideForm();
+            this.editableElement = {};
+        },
+
         _addNewItem: function (item) {
             this.options.list.add(item);
-            $(this.options.body).append(this.jqRepeat([item], this.options.template));
+            this.options.body.append(this.jqRepeat([ item ], this.options.template));
         },
 
         _editCurrentItem: function (item) {
-            this.options.list.add(item, this.index);
-            var existedRow = this.options.body.children().eq(this.index);
+            var index = this.options.list.get().indexOf(this.editableElement);
+            var existedRow = this.options.body.children().eq(index);
+            var formatters = this._formatters;
+
+            this.editableElement = item;
+
             $(existedRow).find('[data-name=title], [data-name=sku], [data-name=price]').each(function (index, element) {
-                var value = item[$.attr(this, 'data-name')];
-                $(this).text(($(this).attr('data-name')==='price')?'$ '+value:value);
+                var fieldName = $.attr(element, 'data-name');
+                var format = formatters[fieldName] || formatters['default'];
+
+                $(element).text(format(item[fieldName]));
             });
+        },
+
+        _formatters: {
+            'default': function (value) {
+                return value
+            },
+
+            price: function (value) {
+                return '$ ' + value;
+            }
         },
 
         closeForm: function(event){
@@ -207,7 +231,7 @@
         },
 
         _hideForm: function () {
-            $(this.itemForm).addClass('invisible');
+            this.itemForm.addClass('invisible');
             this.element.find('.modal-overlay').addClass('invisible');
         }
 
--- JS & jQuery v0.1/js/jq-repeat.js
+++ JS & jQuery v0.1/js/jq-repeat.js
@@ -1,16 +1,10 @@
 (function ($) {
     $.Widget.prototype.jqRepeat = function (items, template) {
-        //var self = this;
-        //console.log(arrElems.constructor);
-        //if(){
-        //
-        //}
         return items.reduce(function (domNodes, item) {
             var itemHtml = $.trim(jqReplace(template, item));
 
             return domNodes.concat($.parseHTML(itemHtml));
         }, []);
-        //$(self).html('').append(resultTmp);
     };
 
     function jqReplace(template, obj) {
